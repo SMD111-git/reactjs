d8f6651f815f5c949d779642ee7a47d594e8cf5f
SMD111-git <owaissmd99@gmail.com>
2024-12-19 20:42:44 +0530

    file understanding and adding function files

--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -1,6 +1,8 @@
 function Chai() {
     return(
-        <h2>chai is Ready </h2>
+        <> <h2>chai is Ready </h2>
+        <p>what is kahan kaha ka jana</p>
+        </>
     )  
 }
 export default Chai
