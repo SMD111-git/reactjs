988e6ffa555d836330596ba6932e06b29a14f94d
SMD111-git <owaissmd99@gmail.com>
2024-12-19 22:09:32 +0530

    file understanding and manupliating  files

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -9,3 +9,18 @@ function App() {
 }
 
 export default App
+
+
+//we can get the chai file  heere with the hlep of
+/*import Chai  from "./chai"
+function App(){
+  return(
+    <chai/>
+  )
+}
+
+export default <App></App>
+/**similairly we can get files to main.jsx in same manner the sturtcure may differe check it in main.jsx 
+ now the key poinnt is that if then a nummber of tag inn the function but the function allow olny one but we have a way to add nummber of tag and
+ to manuplaite  this vitural dom by just wrapping <></>  the whole data under this 
+ */
