ee3d6124180eceb918d0cb102310478f4f59ac36
SMD111-git <owaissmd99@gmail.com>
2024-12-22 23:26:16 +0530

    file adding and understading and write the comment in nextapp (src->index.js,app.js)

--- nextapp/src/chai.js
+++ nextapp/src/chai.js
@@ -1,3 +1,6 @@
-function chai()[
-    
-]
+function Chai(){
+   return(
+    <h1>SMD OWAIS IS NEXT KING AND KING MAKER</h1>
+   ) ;
+}
+export default Chai;
--- nextapp/src/index.js
+++ nextapp/src/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import Chai from './chai';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <!--the app is a customized tag by using file.js or jsx
+    
+    <Chai/>
+  </React.StrictMode>
+);
+/**the app is a customized tag by using file.js or jsx
     and we see that in app.js the file constains a function in that we have a html tag retruning in the function 
     by this method we donot need write whole html tag inside the index by just add file.js and import it to index.js files ,
     this will reflect it into the index.html or the output display, this index.html is loacted in the public folder.
     if we notices the point in above code we are using the get quire to get the acess of index.html file and reload the tags or content into the index.html file, 
-    -->this type of function or coustimzed tags are used to in single page reload and this dirtecly get loaded into the index.html by the automation of script by the package.json filed
-    it get loaded into it., this render the files and load it in index.html files //-->
-  </React.StrictMode>
-);
+    this type of function or coustimzed tags are used to in single page reload and this dirtecly get loaded into the index.html by the automation of script by the package.json filed
+    it get loaded into it., this render the files and load it in index.html files  */
 
