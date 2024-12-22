6571dcbc482d2d035acbb8669451e2ca0fe6f3bb
SMD111-git <owaissmd99@gmail.com>
2024-12-22 23:44:25 +0530

    file adding and understading and write the comment in nextapp (src->index.js,app.js) in viteapp

---
+++ 01vitereact/src/Aur.jsx
@@ -0,0 +1,15 @@
+function Aur() {
+    return(
+        <>
+        <h1>SMD AUR CHAI THE GREAT KING MAKER</h1>
+        <p>this that is thos this those that</p>
+        <img></img>
+        </>/**now this a structure where we can add mutlipe tags in a single function that helps the user add mutlipe tags by using
+         * <>
+         * <h1></h1>
+         * <p></p>
+         * </> nnthis is the structure to do
+         */
+    )
+}
+export default Aur
