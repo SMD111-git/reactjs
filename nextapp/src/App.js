0d9da592302b8ce17e2cb64db2ffa1f1d5145425
SMD111-git <owaissmd99@gmail.com>
2024-12-23 00:02:18 +0530

    file adding and understading and write the comment in nextapp (src->index.js,app.js) in nextapp

--- nextapp/src/App.js
+++ nextapp/src/App.js
@@ -1,9 +1,13 @@
-
-
+import Aur from "./aur";//the aur file is import from other file functio this function and this file impotred in main.js and the first letter of the function
+//must captial when compoents 
 function App() {
   return (
-   <h1>SMD aur react |SMD owais</h1>
+    <>
+    <h1>SMD aur react |SMD owais</h1>
+    <Aur/> 
+    </>
+   /** <AUR/> we are importing the tag from the  where it file  */
   );
 }
 
-export default App;
+export default App;//when create the componet you must the the first letter to inn captial
---
+++ nextapp/src/aur.js
@@ -0,0 +1,9 @@
+function Aur(){
+    return(
+        <>
+        <h2> home in chai is code</h2>
+        <h3>chai aur SMD </h3>
+        </>
+    )
+}
+export default Aur
