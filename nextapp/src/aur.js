e15dc4cea4a91316333ac5e6d8976d7e5588b5e3
smdgit-111 <owaissmd99@gmail.com>
2024-12-27 18:32:23 +0530

    undering the wrappering in the src file of aur.js file

--- nextapp/src/aur.js
+++ nextapp/src/aur.js
@@ -1,5 +1,6 @@
 function Aur(){
-    return(
+    return(// this a way to  write  mulitple statment of line under a single div or block ,
+        //as the react file can olny reperesent one tag at a time so we find it by warpping it into a <div></div> or <></>
         <>
         <h2> home in chai is code</h2>
         <h3>chai aur SMD </h3>
