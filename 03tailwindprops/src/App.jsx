d89727faae6ffbe8d288e1ac2820ae69b1be4334
smdgit-111 <owaissmd99@gmail.com>
2024-12-31 11:08:27 +0530

    tailwind and compontent

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import card from './compoent/compent'
+import Card from './compoents/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-6'>Tailwind test</h1>
-     <card/>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind</h1>
+      <Card/>
     </>
   )
 }
