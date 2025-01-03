77d1301c77f94f86a7ffa49b969adca88ea58dbb
smdgit-111 <owaissmd99@gmail.com>
2025-01-03 10:55:41 +0530

    04bgchanger

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
-      <div className="fixed  flex flex-wrap justify-center bottom-12 inset-1"></div>
+      <div className="fixed  flex flex-wrap justify-center bottom-12 inset-x-0 px-2 ">red</div>
     </div>
     </>
   )
