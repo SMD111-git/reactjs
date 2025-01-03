d162b4c5908de5da3adb4f552c716475b6f0adc8
smdgit-111 <owaissmd99@gmail.com>
2025-01-03 10:52:18 +0530

    04bgchanger

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,11 +1,12 @@
-
-
+import {useState}from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("green")
 
   return (
     <>
-     
+    <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+      <div className="fixed  flex flex-wrap justify-center bottom-12 inset-1"></div>
+    </div>
     </>
   )
 }
