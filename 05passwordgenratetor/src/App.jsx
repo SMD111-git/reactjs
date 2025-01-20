9120a0ae7b03aa0dac45dbdfb212140cf51606ef
SMD111-git <owaissmd99@gmail.com>
2025-01-20 21:22:28 +0530

    commit

--- 05passwordgenratetor/src/App.jsx
+++ 05passwordgenratetor/src/App.jsx
@@ -24,14 +24,19 @@ function App() {
 
   return (
     <>
-    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-100">test</div>
-    <div className='className="flex shadow rounded-lg overflow-hidden  mb-4"'><input 
-    type="text"
-    value={Password} 
-    className="outline-none w-full py-1 px-3"
-    placeholder="Password"
-    
-    /></div>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">text 
+    <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input
+      type='text'
+      value={Password}
+      className='outline-none w-full py-1 px-3'
+      placeholder='password'
+      readOnly
+      />
+      </div>
+    </div>
+
     </>
   )
 }
