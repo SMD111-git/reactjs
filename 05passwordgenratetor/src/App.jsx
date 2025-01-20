cb71fcec6ca42983dc387cc27327631ee9b1085d
SMD111-git <owaissmd99@gmail.com>
2025-01-20 21:23:28 +0530

    changes

--- 05passwordgenratetor/src/App.jsx
+++ 05passwordgenratetor/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState,useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [Lenght, setLenght] = useState(8)
