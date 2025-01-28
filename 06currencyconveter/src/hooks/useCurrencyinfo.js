0af1ac8645a789ef36942a1c1db55bbbb48eed52
SMD111-git <owaissmd99@gmail.com>
2025-01-28 21:50:14 +0530

    currency

---
+++ 06currencyconveter/src/hooks/useCurrencyinfo.js
@@ -0,0 +1,9 @@
+import {useEffect,useState} from "react";
+
+function useCurrencyinfo(currency){
+    useEffect(()=>{
+        fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json')
+        .then((res)=>res.json())
+        .then((res)=>)
+    },[])
+}
