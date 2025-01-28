74d3b3da7fc081e81738ff446b3bb47b05dc599f
SMD111-git <owaissmd99@gmail.com>
2025-01-28 22:46:13 +0530

    currency

--- 06currencyconveter/src/hooks/useCurrencyinfo.js
+++ 06currencyconveter/src/hooks/useCurrencyinfo.js
@@ -1,9 +1,17 @@
 import {useEffect,useState} from "react";
 
 function useCurrencyinfo(currency){
+    const [data,setData]=useEffect({})
     useEffect(()=>{
         fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json')
         .then((res)=>res.json())
-        .then((res)=>)
-    },[])
+        /**.then((res) => res.json()):
+After making a fetch() request, this then() method takes the response (res) and converts it into JSON format using the .json() method. This is typically done because most APIs return data in JSON format.
+
+.then((res) => setdate(res[currency])):
+Once the response is converted into JSON, this second then() is called. The res[currency] accesses a property in the returned JSON object, where currency is likely a variable containing a specific key, such as "USD", "EUR", or some other currency code. Then, it calls a function setdate() with this value. */
+        .then((res)=>setdate(res[currency]))
+        console.log(data);
+        
+    },[currency])
 }
