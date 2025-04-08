c83bfdd0d43e504e520e05fcd2bb83a046dc76b7
SMD111-git <owaissmd99@gmail.com>
2025-04-08 21:23:29 +0530

    app.jsx usecallback

--- passwordgen/src/App.jsx
+++ passwordgen/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
+import { useState ,useCallback} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length ,setLenght] = useState(8)
+  const [numberallowed,setnumberallowed]=useState(false)
+  const [charallowed,setcharallowed]=useState(false)
+  const [password,setpassword]=useState("")
+  const passwordGen=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNQPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberallowed) str+="0123456789"
+    if(charallowed) str+="~!@#$%^&*(){}|":<>?</>"
+    
+  },[length,numberallowed,charallowed,setpassword])
+
+  }
 
   return (
     <>
-      <h1 className='text-4xl text-center'>password generator </h1>
+      <h1 className='text-4xl text-center text-white'>password generator </h1>
     </>
   )
 }
