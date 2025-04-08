b36d937ee12ca49861624143edfa34b05b09b3af
SMD111-git <owaissmd99@gmail.com>
2025-04-08 21:56:45 +0530

    app.jsx usecallback

--- passwordgen/src/App.jsx
+++ passwordgen/src/App.jsx
@@ -13,18 +13,19 @@ function App() {
     let str="ABCDEFGHIJKLMNQPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if(numberallowed) str+="0123456789"
     if(charallowed) str+="~!@#$%^&*()<>?{}"
-    for(let i=1;i<=array.lenght,i++)
+    for(let i=1;i<=array.lenght;i++)
     {
       let char=Math.floor(Math.random()*str.lenght+1)
     }
+    setpassword(pass)
     
   },[length,numberallowed,charallowed,setpassword]) 
 
   }
 
-  return (
+ return (
     <>
-      <h1 className='text-4xl text-center text-white'>password generator </h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-600 bg-gray-600'>test</div>
     </>
   )
 }
