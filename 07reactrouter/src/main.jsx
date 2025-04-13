d23c1f6060627138054effbca23caad7e9bd96b8
SMD111-git <owaissmd99@gmail.com>
2025-04-13 16:53:26 +0530

    editing  compoent

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router=createBrowserRouter([{path:'/'}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
