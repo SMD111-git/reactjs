b408d87ca3a2725cd3410eca282bf8050632f41d
SMD111-git <owaissmd99@gmail.com>
2025-04-13 16:49:30 +0530

    editing  compoent

--- 07reactrouter/src/App.jsx
+++ 07reactrouter/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './componet/footer/footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-600 p-4 '>react</h1>
+     
     </>
   )
 }
