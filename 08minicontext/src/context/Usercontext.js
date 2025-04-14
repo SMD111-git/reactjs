7470c3a18696a2e85ca11cdff9934fea350f24a1
SMD111-git <owaissmd99@gmail.com>
2025-04-14 18:02:10 +0530

    08mini

---
+++ 08minicontext/src/context/Usercontext.js
@@ -0,0 +1,4 @@
+import React from "react";
+const Usercontext=React.createContext()//creating a method context  and storing it in variable
+
+export default Usercontext;//this give you the output of the varaible usercontext
