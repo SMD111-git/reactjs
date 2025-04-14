8b219365194d44b757d9dd366aed5ec9e399a5ec
SMD111-git <owaissmd99@gmail.com>
2025-04-14 21:14:47 +0530

    08mini

--- 08minicontext/src/context/Usercontextprovider.jsx
+++ 08minicontext/src/context/Usercontextprovider.jsx
@@ -2,10 +2,11 @@ import React, { Children } from "react";
 import Usercontext from "./Usercontext";
 
 const Usercontextprovider=({Children})=>{//usecase and is the function
+    const[user,setUser]=React.useState([])
     return(
-        <>
+        <Usercontext.Provider>
         {Children}
-        </>
+        </Usercontext.Provider>
     )
 }
 
