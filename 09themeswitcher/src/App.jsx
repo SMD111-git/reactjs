a156d34aff940b202d9d4aa238b6c0c0530e8946
SMD111-git <owaissmd99@gmail.com>
2025-04-17 23:28:35 +0530

    08mini

---
+++ 09themeswitcher/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='p-4 bg-pink-400 text-3xl'>chai</h2>
+    </>
+  )
+}
+
+export default App
