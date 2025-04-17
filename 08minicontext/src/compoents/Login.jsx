677af625cf70cc0234591beee7d11a39ef7d1834
SMD111-git <owaissmd99@gmail.com>
2025-04-17 15:23:05 +0530

    08mini

--- 08minicontext/src/compoents/Login.jsx
+++ 08minicontext/src/compoents/Login.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React,{useState,useContext} from 'react'; //importing the hooks and compoents in to the file
+import Usercontext from '../context/Usercontext'; //importing the context file in the this app.jsx
+function Login() {
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    const handelSubmit=()=>{
 
-function Login(props) {
+    }
     return (
         <div>
-            
+            <h2>Login</h2>
+            <input type="text" 
+             value={username}
+             onChange={(e)=> setUsername(e.target.value)}placeholder='username'/>
+            <input type="text" placeholder='password'/>
+            <button onClick={handelSubmit}>submit</button>
         </div>
     );
 }
