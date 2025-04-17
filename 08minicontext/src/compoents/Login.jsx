f3659a3e95548564f7fb91c9e26953e1d5b07fb0
SMD111-git <owaissmd99@gmail.com>
2025-04-17 15:35:15 +0530

    08mini

--- 08minicontext/src/compoents/Login.jsx
+++ 08minicontext/src/compoents/Login.jsx
@@ -2,7 +2,8 @@ import React,{useState,useContext} from 'react'; //importing the hooks and compo
 import Usercontext from '../context/Usercontext'; //importing the context file in the this app.jsx
 function Login() {
     const [username,setUsername] = useState('')
-    const [password,setPassword] = useState('')
+    const [password,setpassword] = useState('')
+    const {setUser}=useContext(useContext)
     const handelSubmit=()=>{
 
     }
@@ -10,9 +11,13 @@ function Login() {
         <div>
             <h2>Login</h2>
             <input type="text" 
-             value={username}
-             onChange={(e)=> setUsername(e.target.value)}placeholder='username'/>
-            <input type="text" placeholder='password'/>
+             value={username} //this use the state of usestate and handel the username throught the varaible usestate
+             onChange={(e)=> setUsername(e.target.value)} //this code of line try's to handel the username if there is a change in username 
+             placeholder='username'/>
+            <input type="text"
+             value={password} 
+             onChange={(e)=> setpassword(e.target.value)}
+              placeholder='password'/>
             <button onClick={handelSubmit}>submit</button>
         </div>
     );
