71218b51d7687585c6617b4fda44bb688c466e1b
SMD111-git <owaissmd99@gmail.com>
2025-04-17 17:11:18 +0530

    08commit

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return ( 
     <UserContextProvider>
-      <h1>SMD THE UPCOMING EMPIER</h1>
+      <h1>SIKE UPCOMING SOON</h1>
       <Login/>
       <Profile/>
     </UserContextProvider>
