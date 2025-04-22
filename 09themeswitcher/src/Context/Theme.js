a4ada2ee1aa65782fbc67b44c602560f23917364
SMD111-git <owaissmd99@gmail.com>
2025-04-22 14:05:32 +0530

    09themeswitcher

--- 09themeswitcher/src/Context/Theme.js
+++ 09themeswitcher/src/Context/Theme.js
@@ -1,7 +1,7 @@
 import {createContext,useContext} from "react";
 
-const ThemeContext = createContext({
+export const ThemeContext = createContext({
     themeMode:"light",
     darkTheme:()=>{}
 })
-export default ThemeContext
+export 
