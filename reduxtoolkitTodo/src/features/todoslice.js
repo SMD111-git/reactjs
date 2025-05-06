87d1eb5d49e4dae54512aac082c45f8e71bc5c2f
SMD111-git <owaissmd99@gmail.com>
2025-05-06 20:43:35 +0530

    reduxtoolkittodo

--- reduxtoolkitTodo/src/features/todoslice.js
+++ reduxtoolkitTodo/src/features/todoslice.js
@@ -1,1 +1,21 @@
-import{createSlice,nanoid} from 'reduxjs/toolkit'
+import{createSlice,nanoid} from '@reduxjs/toolkit';
+
+const initialState={
+    todos:[{id:1,text:"hello world"}]
+}
+
+
+export const todoSlice =createSlice({
+    name:'todo',
+    initialState,
+    reducers:{
+        addtodo:(state,action)=>{
+            const todo={ //this todo is created , to updaate the state
+                id:nanoid(),
+                text:action.payload //here payload get all objects,email other things
+            }
+            state.todo.push(todo)
+        },
+        removeTodo:()=>{}
+    } //reducers includes prptery and functions
+})
