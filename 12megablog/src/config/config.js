fbe4dc58e14a7e445c87e81f7b5865edf331774a
SMD111-git <owaissmd99@gmail.com>
2025-05-19 17:24:11 +0530

    12megblog

---
+++ 12megablog/src/config/config.js
@@ -0,0 +1,11 @@
+const conf={
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID:String(import.meta.env.VITE_Project_ID),
+    appwriteDatabaseID:String(import.meta.env.VITE_Database_ID),
+    appwriteCollectionID:String(import.meta.env.VITE_Collection_ID),
+    appwriteBUCKETID:String(import.meta.env.VITE_BUCKET_ID)
+
+}
+
+
+export default conf
