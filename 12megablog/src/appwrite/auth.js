599607412d9536520359eb77ac55d47e9e0cd7d4
SMD111-git <owaissmd99@gmail.com>
2025-05-20 14:34:06 +0530

    12megablog

--- 12megablog/src/appwrite/auth.js
+++ 12megablog/src/appwrite/auth.js
@@ -1,2 +1,27 @@
 import config from '../config.js';
+import{Client,Account,ID} from "appwrite";
 
+export class AuthService{
+    Client =new Client();
+    account;
+    constructor(){
+        this.Client
+            .setEndpoint(config.appwriteUrl)
+            .setProject(config.appwriteProjectID);
+            this.account=new Account(this.Client);
+    }
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name);
+            if (userAccount) {
+                return userAccount;
+            } else {
+                return 
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+const authService= new AuthService();
+export default authService
