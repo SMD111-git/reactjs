2bb33b3f8d3a1e49a9926025ab4a64ef4d21d664
SMD111-git <owaissmd99@gmail.com>
2025-05-21 10:40:43 +0530

    12megablog

--- 12megablog/src/App.jsx
+++ 12megablog/src/App.jsx
@@ -4,8 +4,10 @@ import { useState,useEffect } from 'react'
 import './App.css'
 import authService from './appwrite/auth'
 import { login,logout } from './store/authSlice' 
+import { Footer,Header } from './compontent'
+import { Outlet } from 'react-router-dom'
 function App() {
-  '''console.log(import.meta.env.VITE_APPWRITE_URL); //this method to access the varaible or other cerditional from .env  file'''
+  //console.log(import.meta.env.VITE_APPWRITE_URL); //this method to access the varaible or other cerditional from .env  file
   const [loading,setLoading] = useState(true)
   const dispatch = useDispatch()
   useEffect(() => {
@@ -19,12 +21,20 @@ function App() {
       }
     })
     .finally(()=>setLoading (false))
-  }, [ ])
+  }, [])
   
   
 
   return !loading ?(
-    <div className='min'></div>
+    <div className='min-h-screen flex- flex-warp content-between bg-gary-400'>
+      <div className='w-full block'>
+        <Headers/>
+        <main>
+          TODO:{/*<Outlet/>*/}
+        </main>
+        <Footer/>
+      </div>
+    </div>
   ): (null)
 }
 
