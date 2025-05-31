3b7b97ef37034397cde62cbf4ddc14200d1bdc37
SMD111-git <owaissmd99@gmail.com>
2025-05-31 18:45:05 +0530

    12mega project

--- 12megablog/src/compontent/Select.jsx
+++ 12megablog/src/compontent/Select.jsx
@@ -17,10 +17,15 @@ function Select({
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         >
-         
+        /**point to taken  */
+         {options?.map((option)=>(
+            <option key={option} value={option}>
+                {option}
+            </option>
+         ))}
         </Select>
     </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
