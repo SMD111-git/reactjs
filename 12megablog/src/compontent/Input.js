d370f5a8888ec653466ab0a81bc9c163301a0c90
SMD111-git <owaissmd99@gmail.com>
2025-05-31 16:25:46 +0530

    12mega project

--- 12megablog/src/compontent/Input.js
+++ 12megablog/src/compontent/Input.js
@@ -6,8 +6,24 @@ const Input= React.forwardRef(function Input({
     className="",
     ...props
 },ref) {
+    const id=useId()
     return (
-        <h1>TRST</h1>
+        <div className='w-full'>
+            {label && <label
+        className='inline-block mb-1 pl-1 ' 
+        htmlFor={id}>
+            {label}
+            </label>
+            }
+            <input
+            type={type}
+            className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            ref={ref}
+            {...props}
+            id={id}
+            />
+
+        </div>
 
     )
 })
