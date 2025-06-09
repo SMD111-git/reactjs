b16123a0ed59fd033db28dca8a2b0fbfa8e769a4
SMD111-git <owaissmd99@gmail.com>
2025-06-09 20:52:42 +0530

    12mega project

--- 12megablog/src/compontent/Header/AuthLayout.jsx
+++ 12megablog/src/compontent/Header/AuthLayout.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-function AuthLayout() {
+export default function Protected({children, authentication = true}) {
+    const navigate =useNavigate()
+    const [loading,setloading]=useState(true)
+    const authStatus =useSelector(state =>state.authStatus)
+    useEffect(()=>{
+        if(authentication && authStatus !== authentication){
+            navigate("/login")
+        }
+        elseif(!authentication && authStatus!==authentication){
+            
+        }
+    },[authStatus,navigate,authentication])
   return (
     <div>AuthLayout</div>
   )
 }
-
-export default AuthLayout
