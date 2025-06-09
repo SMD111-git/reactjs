f79553fba152ac9f2511211f7d5fb9b717d05b7a
SMD111-git <owaissmd99@gmail.com>
2025-06-09 19:20:42 +0530

    12mega project

--- 12megablog/src/compontent/Singup.jsx
+++ 12megablog/src/compontent/Singup.jsx
@@ -1,15 +1,30 @@
 import { useState } from "react"
 import React from 'react'
 import authService from "../appwrite/auth"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import {login} from "..store/authService"
-import {Button,Input, Logo}  from './index'
+import {Button,Input, Logo}  from './index.js'
 import { useDispatch } from "react-redux"
 import { useForm } from "react-hook-form"
 function Singup() {
-  return (
-    <div>Singup</div>
-  )
+  const navigate = useNavigate()
+  const [error,setError] =useState("")
+  const dispatch =useDispatch()
+  const {register,handelSubmit}= useForm()
+   const create = async(data) => {
+        setError("")
+        try {
+            const userData = await authService.createAccount(data)
+            if (userData) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(login(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+
 }
 
 export default Singup
