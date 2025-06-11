fd0d8b79196cf5a9743e754d6c696f7b809c8495
SMD111-git <owaissmd99@gmail.com>
2025-06-11 23:33:26 +0530

    12mega project

--- 12megablog/src/compontent/postform/Postform.jsx
+++ 12megablog/src/compontent/postform/Postform.jsx
@@ -1,16 +1,30 @@
-import { useCallback } from "react"
 import React  from 'react'
+import { useCallback } from "react"
 import { useForm } from "react-hook-form"
 import {Button,Input,Select,RTE} from '../index'
 import appwriteService from '../../appwrite/conf'
-import { useNavigate } from "react-router-dom"
+import { data, useNavigate } from "react-router-dom"
 import { useSelector } from "react-redux"
-function Postform() {
+function PostForm({post}) {
     const {register,handleSubmit,watch,setValue,getValues }=useForm({
         defaultValues:{
-            title:''
+            title:post?.title||'',
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active", 
         },
     }) //this functionaly give varies function to handel  
+    const navigate =useNavigate()
+    const userData =useSelector(state=>state.user.userData)
+    const submit = async(data)=>{
+      if(post){
+       const file= data.image[0]? appwriteService.uploadfile(data.image[0]):null
+       if(file){
+        appwriteService.deleteFile(post.featuredImage)
+        
+       }
+      }
+    }
   return (
     <div>Postform</div>
   )
